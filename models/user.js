b742728cc27d0b4662f9d500dac06578d99d749e
antathm <anta.thiam@edu.devinci.fr>
2024-12-19 22:24:25 +0000

    start

--- models/user.js
+++ models/user.js
@@ -2,10 +2,19 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
-    name: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, unique: true, allowNull: false },
-    password: { type: DataTypes.STRING, allowNull: false },
-    role: { type: DataTypes.STRING, defaultValue: 'user' }
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.STRING,
+        defaultValue: 'user',
+    },
 });
 
 module.exports = User;
